876eaadb023086e09f687b43108efca55d1c1fc1
Dominikodz <64926921+Dominikodz@users.noreply.github.com>
2023-03-25 11:50:23 +0100

    Add plugin

---
+++ NitroStreams.plugin.js
@@ -0,0 +1,56 @@
+/**
+ * @name NitroStreams
+ * @author Domson
+ * @description Allows you to stream in Discord's Nitro quality.
+ * @version 1.1.1
+ */
+/*@cc_on
+@if (@_jscript)
+    
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
+
+@else@*/
+/*
+ * Copyright © 2019-2022, Domson
+ * All rights reserved.
+ * Code may not be redistributed, modified or otherwise taken without explicit permission.
+ */
+
+module.exports = class ExamplePlugin {
+    start() {
+        // how do i do it? // window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().premiumType = 2;}}}]);
+    
+        console.log(
+            window.webpackChunkdiscord_app.push([
+                [Math.random()], {}, (req) => {
+                    for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {
+                        if (m.default && m.default.getCurrentUser !== undefined) {
+                            return m.default.getCurrentUser().premiumType = 2;
+                        }
+                    }
+                }
+            ])
+        );
+    }
+    stop() {
+      // Cleanup when disabled
+    }
+};
+/*@end@*/
